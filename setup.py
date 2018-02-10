15c8d4bd44ff4c44f05001362ab16ff5ada740a8
Jon Grace-Cox <jongracecox@gmail.com>
2018-02-09 21:37:03 -0500

    Added reST version of REAMD.md to package.

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
-
 import mister_bump
 from setuptools import setup
+from m2r import parse_from_file
 
 setup(
     name='pylint-exit',
     description='Exit code handler for pylint command line utility.',
+    long_description=parse_from_file('README.md'),
     version=mister_bump.bump(style='rc'),
     author='Jon Grace-Cox',
     author_email='jongracecox@gmail.com',
     py_modules=['pylint_exit'],
-    setup_requires=['setuptools', 'wheel'],
+    setup_requires=['setuptools', 'wheel', 'm2r'],
     tests_require=[],
     install_requires=['bitarray'],
     data_files=[],
