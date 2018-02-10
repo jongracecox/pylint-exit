b37ba2c875dcb6d4dec9269d9f3d4a03dd55f524
Jon Grace-Cox <jongracecox@gmail.com>
2018-02-10 12:22:06 -0500

    Added README syntax check to setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,25 @@
 import mister_bump
 from setuptools import setup
 from m2r import parse_from_file
+import restructuredtext_lint
+
+
+# Parser README.md into reStructuredText format
+rst_readme = parse_from_file('README.md')
+
+# Validate the README, checking for errors
+errors = restructuredtext_lint.lint(rst_readme)
+
+# Raise an exception for any errors found
+if errors:
+    print(rst_readme)
+    raise ValueError('README.md contains errors: ',
+                     ', '.join([e.message for e in errors]))
 
 setup(
     name='pylint-exit',
     description='Exit code handler for pylint command line utility.',
-    long_description=parse_from_file('README.md'),
+    long_description=rst_readme,
     version=mister_bump.bump(style='rc'),
     author='Jon Grace-Cox',
     author_email='jongracecox@gmail.com',
