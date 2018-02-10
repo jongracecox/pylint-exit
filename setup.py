b0dc16b319d12bbdf26209ab1255503ac13bb53d
Jon Grace-Cox <jongracecox@gmail.com>
2018-02-09 21:29:46 -0500

    Switch to using mister-bump for version bumping. Test using Python 3.5, and only deploy 3.6 universal wheel.

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python
 
-import sys
-import re
+import mister_bump
 from setuptools import setup
-
-def get_version():
-    with open("pylint_exit.py", "r") as file_handle:
-        file_text = file_handle.read()
-    version_match = re.search(r'.* __version__ = [\'"]([^\'"]*)[\'"]', file_text)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to identify version string.")
 
 setup(
     name='pylint-exit',
     description='Exit code handler for pylint command line utility.',
-    version=get_version(),
+    version=mister_bump.bump(style='rc'),
     author='Jon Grace-Cox',
     author_email='jongracecox@gmail.com',
-    py_modules = ['pylint_exit'],
+    py_modules=['pylint_exit'],
     setup_requires=['setuptools', 'wheel'],
     tests_require=[],
     install_requires=['bitarray'],
@@ -27,7 +18,7 @@ setup(
         'bdist_wheel': {'universal': True}
     },
     url='https://github.com/jongracecox/pylint-exit',
-    entry_points = {
+    entry_points={
         'console_scripts': ['pylint-exit=pylint_exit:main'],
     }
 )
